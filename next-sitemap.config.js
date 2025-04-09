f20fcdd5ab510b808657e6790e5d6de2a93fabed
PisethPanha <yornpisethpanha58@gmail.com>
2025-04-09 23:24:16 +0700

    change title

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://rpitst-next.vercel.app', // change to your real domain
+    generateRobotsTxt: true,
+  };
+  
